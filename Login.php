bd002a7b3017f91c0ce7f629a75abc6379c69dbe
masoudesmi <m.esmi@outlook.com>
2018-11-01 23:55:01 +0330

    'forlogin'

--- Login.php
+++ Login.php
@@ -57,7 +57,7 @@
 					<div class="contact100-form-checkbox">
 						<input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
 						<label class="label-checkbox100" for="ckb1">
-							Remember me
+							Remember ME
 						</label>
 					</div>
 
